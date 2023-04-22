832691adda5e0973b8fcd0337b57bcb2f86d437a
Daniel <itzdanielmichael@gmail.com>
2023-04-21 23:50:02 -0400

    update ball handler

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // don't run the rest if we're not touching the touch screen
+    if (!IsTouchScreenPressed())
     {
-        
+      return;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // get current touch position
+    Vector2 touchPosition = GetCurrentTouchPosition();
+
+    Debug.Log(touchPosition);
+  }
+
+  Vector2 GetCurrentTouchPosition()
+  {
+    return Touchscreen.current.primaryTouch.position.ReadValue();
+  }
+
+  bool IsTouchScreenPressed()
+  {
+    return Touchscreen.current.primaryTouch.press.isPressed;
+  }
 }
